4f0cfc87ea416b291754b203946e3ceb00b00179
Tom Christian <tom@sparkgeo.com>
2024-06-13 11:39:17 -0700

    feat(#8): implemented S3 locations for test data

--- data/STAC/pc_partial_download.py
+++ data/STAC/pc_partial_download.py
@@ -1,33 +1,48 @@
 from json import dump
 from os import makedirs, path
+from re import sub
 from typing import Final, List
 
 import requests
 
-base_url: Final[str] = "https://planetarycomputer.microsoft.com/api/stac/v1/"
+
+def s3_join(parts: List[str]) -> str:
+    return "/".join([sub("/$", "", sub("^/", "", part)) for part in parts])
+
+
+base_source_url: Final[str] = "https://planetarycomputer.microsoft.com/api/stac/v1/"
 collection_ids: Final[List[str]] = [
     "ecmwf-forecast",
     "modis-17A2HGF-061",
 ]
 json_type: Final[str] = "application/json"
 geojson_type: Final[str] = "application/geo+json"
+s3_base_url: Final[str] = "s3://tchristian-stac-serverless-data/pc_partial_data"
 
-base_data_path: Final[str] = path.abspath(
-    path.join(path.dirname(__file__), "pc_partial_data")
+base_source_path: Final[str] = path.abspath(
+    path.join(path.dirname(__file__), "pc_partial_data", "s3")
 )
-catalog_path: Final[str] = path.join(base_data_path, "catalog.json")
+catalog_path: Final[str] = s3_join([s3_base_url, "catalog.json"])
 
 
-def collection_file_path(collection_id: str) -> str:
-    return path.join(base_data_path, "collections", "{}.json".format(collection_id))
+def collection_source_path(collection_id: str) -> str:
+    return path.join(base_source_path, "collections", "{}.json".format(collection_id))
 
 
-def collection_items_dir(collection_id: str) -> str:
-    return path.join(base_data_path, "collections", collection_id, "items")
+def collection_access_path(collection_id: str) -> str:
+    return s3_join([s3_base_url, "collections", collection_id])
+
+
+def collection_items_source_path(collection_id: str) -> str:
+    return path.join(base_source_path, "collections", collection_id, "items")
+
+
+def collection_items_access_path(collection_id: str) -> str:
+    return s3_join([s3_base_url, "collections", collection_id, "items"])
 
 
 def execute():
-    with open(path.join(base_data_path, "catalog.json"), "w") as f:
+    with open(path.join(base_source_path, "catalog.json"), "w") as f:
         dump(
             {
                 "type": "Catalog",
@@ -40,12 +55,12 @@ def execute():
                     {
                         "rel": "self",
                         "type": json_type,
-                        "href": f"file://{catalog_path}",
+                        "href": catalog_path,
                     },
                     {
                         "rel": "root",
                         "type": json_type,
-                        "href": f"file://{catalog_path}",
+                        "href": catalog_path,
                     },
                 ]
                 + [
@@ -53,7 +68,7 @@ def execute():
                         "rel": "child",
                         "type": json_type,
                         "title": collection_id,
-                        "href": f"file://{collection_file_path(collection_id)}",
+                        "href": collection_access_path(collection_id),
                     }
                     for collection_id in collection_ids
                 ],
@@ -64,7 +79,9 @@ def execute():
 
     for collection_id in collection_ids:
         print(f"fetching collection {collection_id}")
-        collection_response = requests.get(f"{base_url}/collections/{collection_id}")
+        collection_response = requests.get(
+            f"{base_source_url}/collections/{collection_id}"
+        )
         if collection_response.status_code != 200:
             print(
                 f"problem fetching {collection_id}: {collection_response.status_code}"
@@ -75,7 +92,6 @@ def execute():
         except Exception as e:
             print(f"problem parsing {collection_id}: {e}")
             continue
-        collection_path = collection_file_path(collection_id)
         collection = {
             **collection,
             **{
@@ -92,35 +108,35 @@ def execute():
                     {
                         "rel": "items",
                         "type": geojson_type,
-                        "href": f"file://{collection_items_dir(collection_id)}",
+                        "href": collection_items_access_path(collection_id),
                     },
                     {
                         "rel": "parent",
                         "type": json_type,
-                        "href": f"file://{catalog_path}",
+                        "href": catalog_path,
                     },
                     {
                         "rel": "root",
                         "type": json_type,
-                        "href": f"file://{catalog_path}",
+                        "href": catalog_path,
                     },
                     {
                         "rel": "self",
                         "type": json_type,
-                        "href": f"file://{collection_path}",
+                        "href": collection_access_path(collection_id),
                     },
                 ]
             },
         }
-        makedirs(collection_items_dir(collection_id), exist_ok=True)
-        with open(collection_path, "w") as f:
+        makedirs(collection_items_source_path(collection_id), exist_ok=True)
+        with open(collection_source_path(collection_id), "w") as f:
             dump(
                 collection,
                 f,
                 indent=2,
             )
 
-        next_items_url = f"{base_url}/collections/{collection_id}/items"
+        next_items_url = f"{base_source_url}/collections/{collection_id}/items"
         page_count = 0
         while next_items_url is not None:
             page_count += 1
@@ -145,10 +161,6 @@ def execute():
                 next_link_entries[0]["href"] if len(next_link_entries) == 1 else None
             )
             for feature in items["features"]:
-                item_path = path.join(
-                    collection_items_dir(collection_id),
-                    "{}.json".format(feature["id"]),
-                )
                 feature = {
                     **feature,
                     **{
@@ -166,28 +178,39 @@ def execute():
                             {
                                 "rel": "collection",
                                 "type": json_type,
-                                "href": f"file://{collection_file_path(collection_id)}",
+                                "href": collection_access_path(collection_id),
                             },
                             {
                                 "rel": "parent",
                                 "type": json_type,
-                                "href": f"file://{collection_file_path(collection_id)}",
+                                "href": collection_access_path(collection_id),
                             },
                             {
                                 "rel": "root",
                                 "type": json_type,
-                                "href": f"file://{catalog_path}",
+                                "href": catalog_path,
                             },
                             {
                                 "rel": "self",
                                 "type": geojson_type,
-                                "href": f"file://{item_path}",
+                                "href": s3_join(
+                                    [
+                                        collection_items_access_path(collection_id),
+                                        "{}.json".format(feature["id"]),
+                                    ]
+                                ),
                             },
                         ]
                     },
                 }
 
-                with open(item_path, "w") as f:
+                with open(
+                    path.join(
+                        collection_items_source_path(collection_id),
+                        "{}.json".format(feature["id"]),
+                    ),
+                    "w",
+                ) as f:
                     dump(
                         feature,
                         f,
