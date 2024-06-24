41f21e6b5e05fb21ff848c0e82703cc0b87ada66
Tom Christian <tom@sparkgeo.com>
2024-06-23 17:57:10 -0700

    feat(#22): re-indexing PC data after index design change

--- scripts/data-management/index-stac-file-data.sh
+++ scripts/data-management/index-stac-file-data.sh
@@ -4,5 +4,5 @@ set -e
 
 pushd $(dirname $0)/../..
 index_dir=$(pwd)/data/index/s3
-stac_index_index_output_dir=$index_dir python -m stac_indexer.index s3://tchristian-stac-serverless-data/pc_partial_data/catalog.json
+stac_index_index_output_dir=$index_dir python -m stac_indexer.index ./data/STAC/pc_partial_data/index-config.json
 index_output_dir=$index_dir scripts/data-management/parquet-to-geoparquet.sh
