8bfcfe53d8e8e9d8b38b903fa5f74da38122fe7d
George Pyne <georgepyne@hotmail.co.uk>
2024-07-03 14:28:47 -0700

    feat(#32) AWS Lambda deployment

--- iac/cdk_deployment/cdk_deployment_stack.py
+++ iac/cdk_deployment/cdk_deployment_stack.py
@@ -44,7 +44,7 @@ class CdkDeploymentStack(Stack):
             handler=stac_serverless_lambda,
             default_cors_preflight_options=cors,
             proxy=True,
-            binary_media_types=["application/json"],
+            binary_media_types=["*/*"],
             rest_api_name="STAC-API-Serverless",
         )
         LogGroup(
