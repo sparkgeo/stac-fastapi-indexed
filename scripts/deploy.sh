9539e9632721d426d03ac76a9b8c4dd8bb715f29
George Pyne <georgepyne@hotmail.co.uk>
2024-07-11 13:14:58 -0700

    feat(#32) add cdk context vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd iac
-source .venv/bin/activate
-cdk bootstrap
-cdk deploy
+set -e
+
+pushd $(dirname $0)/..
+
+sh scripts/tests/smoke-test.sh
+
+pushd $(dirname $0)/../iac
+
+cdk deploy -c PARQUET_URI=PARQUET_URI -c JWT_SECRET=$(openssl rand -base64 32) -c LOG_LEVEL=${LOG_LEVEL:-info} -c BOTO_DEBUG=${BOTO_DEBUG:-false} -c DUCKDB_THREADS=${DUCKDB_THREADS:-}
+
+
+
+
+
