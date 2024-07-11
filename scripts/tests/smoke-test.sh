d97e295df01c0108511622a46e562484b7f4fd02
Tom Christian <tom@sparkgeo.com>
2024-07-11 16:28:50 -0700

    feat(#58): remove test containers after use

--- scripts/tests/smoke-test.sh
+++ scripts/tests/smoke-test.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)/../..
 dco="docker compose -f docker-compose.base.yml -f docker-compose.local-s3.yml -f docker-compose.local-s3.test.yml"
 
 $dco build
-$dco run tester python -m pytest -k smoke_tests
+$dco run --rm tester python -m pytest -k smoke_tests
 exit_code=$?
 if [ $exit_code -ne 0 ] || [ ${TESTS_DEBUG:-0} -ne 0 ]; then
     $dco logs
