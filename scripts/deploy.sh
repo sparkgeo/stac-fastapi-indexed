c4ac7dc9c6514efe80cf3ede0308a4452858d203
George Pyne <georgepyne@hotmail.co.uk>
2024-07-12 09:28:22 -0700

    feat(#32) add smoke test to deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ set -e
 
 pushd $(dirname $0)/..
 
-sh scripts/tests/smoke-test.sh
+dco="docker compose -f docker-compose.base.yml -f docker-compose.local-s3.yml -f docker-compose.local-s3.smoke-test.yml"
+
+$dco build
+$dco run tester
+$dco logs
+$dco down
 
 pushd $(dirname $0)/../iac
 
 cdk deploy -c PARQUET_URI=PARQUET_URI -c JWT_SECRET=$(openssl rand -base64 32) -c LOG_LEVEL=${LOG_LEVEL:-info} -c BOTO_DEBUG=${BOTO_DEBUG:-false} -c DUCKDB_THREADS=${DUCKDB_THREADS:-}
-
-
-
-
-
