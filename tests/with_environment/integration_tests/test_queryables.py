46c42efdf422367efd2c799c1d20f59bf20178f3
Tom Christian <64801328+captaincoordinates@users.noreply.github.com>
2024-07-17 15:53:46 -0700

    feat(#58): integration tests (missing filter)
    * feat(#58): restructured tests for reusability
    
    * feat(#58): first integration test
    
    * feat(#58): added to PR GHA workflow
    
    * feat(#58): collections integration tests
    
    * feat(#58): collection integration test
    
    * feat(#58): remove test containers after use
    
    * feat(#58): collection items integration test
    
    * feat(#58): added queryables integration test
    
    * feat(#58): added item integration test
    
    * feat(#58): post search integration test
    
    * feat(#58): get search integration test
    
    * feat(#58): removed unnecessary log messages
    
    * feat(#58): moved minio config to more appropriate location
    
    * feat(#58): DRY results comparison

---
+++ tests/with_environment/integration_tests/test_queryables.py
@@ -0,0 +1,45 @@
+from json import load
+from typing import Final, List
+
+import requests
+from with_environment.common import api_base_url
+from with_environment.integration_tests.common import get_index_config_path
+from with_environment.wait import wait_for_api
+
+_global_queryables: Final[List[str]] = [
+    "id",
+    "collection",
+    "geometry",
+    "datetime",
+    "datetime_end",
+]
+
+
+def setup_module():
+    wait_for_api()
+
+
+def test_queryables_all_collections():
+    queryable_properties = requests.get(f"{api_base_url}/queryables").json()[
+        "properties"
+    ]
+    assert len(queryable_properties.keys()) == len(_global_queryables)
+    for expected_property_name in _global_queryables:
+        assert expected_property_name in queryable_properties
+
+
+def test_queryables_by_collection():
+    with open(get_index_config_path(), "r") as f:
+        index_config = load(f)
+    for collection_id, queryable_config in index_config["queryables"][
+        "collection"
+    ].items():
+        expected_queryables = list(queryable_config.keys())
+        collection_queryable_properties = requests.get(
+            f"{api_base_url}/collections/{collection_id}/queryables"
+        ).json()["properties"]
+        assert len(collection_queryable_properties.keys()) == len(
+            _global_queryables + expected_queryables
+        )
+        for expected_property_name in _global_queryables + expected_queryables:
+            assert expected_property_name in collection_queryable_properties
