ce574a847bb730550aa24ed839ac07a64c709bbe
Tom Christian <64801328+captaincoordinates@users.noreply.github.com>
2024-09-11 17:01:41 -0700

    feat(#83): added and tested filesystem reader
    * feat(#83): added basic filesystem reader
    
    * feat(#83): default to /data root in STAC, root to minio for testing
    
    * feat(#83): clean up now-unnecessary directories
    
    * feat(#83): clean-up unnecessary docker-compose content
    
    * feat(#83): extended tests to include local-file reader

--- scripts/dev-init.sh
+++ scripts/dev-init.sh
@@ -5,6 +5,7 @@ set -e
 pushd $(dirname $0)/..
 
 pip install -e ./stac_index/common[dev]
+pip install -e ./stac_index/reader/filesystem[dev]
 pip install -e ./stac_index/reader/s3[dev]
 pip install -e ./stac_index/indexer[dev]
 pip install -e ./stac_fastapi/indexed[dev,server,test]
