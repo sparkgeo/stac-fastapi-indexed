b08378d1355b47c99e4e52168d17846c4f1e239b
Tom Christian <thomaschristian@gmail.com>
2024-09-23 14:30:33 -0700

    feat(#97): support static http tests

--- data/STAC/http/sample_to_http_static.py
+++ data/STAC/http/sample_to_http_static.py
@@ -1,19 +1,19 @@
 from glob import glob
 from json import dump, load
 from os import makedirs, path
-from typing import Final
+from shutil import copyfile
+from typing import Final, List
 
 source_root_default: Final[str] = path.join(path.dirname(__file__), "sample", "data")
 target_root_default: Final[str] = path.join(path.dirname(__file__), "sample-alt")
 
 
 def main(
-    old_link_prefix: str,
-    new_link_prefix: str,
     source_root: str,
     target_root: str,
 ) -> None:
-    with open(path.join(source_root, "catalog.json"), "r") as f:
+    catalog_in_path = path.join(source_root, "catalog.json")
+    with open(catalog_in_path, "r") as f:
         catalog = load(f)
 
     for child_link_dict in [
@@ -21,7 +21,7 @@ def main(
     ]:
         collection_in_path = path.join(
             source_root,
-            *str(child_link_dict["href"]).replace(old_link_prefix, "").split("/"),
+            child_link_dict["href"],
         )
 
         with open(collection_in_path, "r") as f:
@@ -31,6 +31,7 @@ def main(
                 print(f"failed to open collection '{collection_in_path}'")
                 continue
 
+        collection_item_links: List[str] = []
         for items_link_dict in [
             entry for entry in collection["links"] if entry["rel"] == "items"
         ]:
@@ -38,9 +39,7 @@ def main(
                 path.join(
                     path.join(
                         source_root,
-                        *str(items_link_dict["href"])
-                        .replace(old_link_prefix, "")
-                        .split("/"),
+                        items_link_dict["href"],
                     ),
                     "*.json",
                 )
@@ -51,27 +50,12 @@ def main(
                     except Exception:
                         print(f"failed to open item '{item_in_path}'")
                         continue
-
+                collection_item_links.append(
+                    [link["href"] for link in item["links"] if link["rel"] == "self"][0]
+                )
                 item_out_path = item_in_path.replace(source_root, target_root)
                 makedirs(path.dirname(item_out_path), exist_ok=True)
-
-                with open(item_out_path, "w") as f:
-                    dump(
-                        {
-                            **item,
-                            "links": [
-                                {
-                                    **link,
-                                    "href": str(link["href"]).replace(
-                                        old_link_prefix, new_link_prefix
-                                    ),
-                                }
-                                for link in item["links"]
-                            ],
-                        },
-                        f,
-                        indent=2,
-                    )
+                copyfile(item_in_path, item_out_path)
 
         collection_out_path = collection_in_path.replace(source_root, target_root)
         makedirs(path.dirname(collection_out_path), exist_ok=True)
@@ -80,13 +64,15 @@ def main(
                 {
                     **collection,
                     "links": [
+                        link for link in collection["links"] if link["rel"] != "items"
+                    ]
+                    + [
                         {
-                            **link,
-                            "href": str(link["href"]).replace(
-                                old_link_prefix, new_link_prefix
-                            ),
+                            "rel": "item",
+                            "type": "application/json",
+                            "href": item_link,
                         }
-                        for link in collection["links"]
+                        for item_link in collection_item_links
                     ],
                 },
                 f,
@@ -95,39 +81,13 @@ def main(
 
     catalog_out_path = path.join(target_root, "catalog.json")
     makedirs(path.dirname(catalog_out_path), exist_ok=True)
-    with open(catalog_out_path, "w") as f:
-        dump(
-            {
-                **catalog,
-                "links": [
-                    {
-                        **link,
-                        "href": str(link["href"]).replace(
-                            old_link_prefix, new_link_prefix
-                        ),
-                    }
-                    for link in catalog["links"]
-                ],
-            },
-            f,
-            indent=2,
-        )
+    copyfile(catalog_in_path, catalog_out_path)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument(
-        "old_link_prefix",
-        type=str,
-        help="STAC link path prefix to replace",
-    )
-    parser.add_argument(
-        "new_link_prefix",
-        type=str,
-        help="Replacement for STAC link path prefix",
-    )
     parser.add_argument(
         "--source_root",
         type=str,
@@ -144,8 +104,6 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     main(
-        args.old_link_prefix,
-        args.new_link_prefix,
         args.source_root[0],
         args.target_root[0],
     )
