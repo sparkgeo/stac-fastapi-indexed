3d70ae5ec3a6f0c6c4760265fc1f874d46658ae9
Tom Christian <thomaschristian@gmail.com>
2024-10-12 18:24:50 -0700

    feat(#84): removed spatial optimisation on intersect queries

--- stac_fastapi/indexed/stac_fastapi/indexed/search/spatial.py
+++ stac_fastapi/indexed/stac_fastapi/indexed/search/spatial.py
@@ -1,13 +1,14 @@
-from shapely.wkt import loads as loads_wkt
-
 from stac_fastapi.indexed.search.filter_clause import FilterClause
 
 
 def get_intersects_clause_for_wkt(wkt: str) -> FilterClause:
-    geometry = loads_wkt(wkt)
-    return _get_intersects_clause_for_wrapped_geometry(
-        f"ST_GeomFromText('{wkt}')",
-        *geometry.bounds,
+    return FilterClause(
+        sql=f"""
+            ST_Intersects(
+                geometry,
+                ST_GeomFromText('{wkt}')
+            )
+        """
     )
 
 
@@ -16,29 +17,4 @@ def get_intersects_clause_for_bbox(
 ) -> FilterClause:
     return get_intersects_clause_for_wkt(
         f"POLYGON (({x_min} {y_min}, {x_max} {y_min}, {x_max} {y_max}, {x_min} {y_max}, {x_min} {y_min}))"
-    )
-
-
-def _get_intersects_clause_for_wrapped_geometry(
-    sql_wrapped_geometry: str, x_min: float, y_min: float, x_max: float, y_max: float
-) -> FilterClause:
-    return FilterClause(
-        sql=f"""
-            NOT (
-                    bbox_x_max < ?
-                    OR bbox_y_max < ?
-                    OR bbox_x_min > ?
-                    OR bbox_y_min > ?
-                )
-            AND ST_Intersects(
-                    geometry,
-                    {sql_wrapped_geometry}
-                )
-        """,
-        params=[
-            x_min,
-            y_min,
-            x_max,
-            y_max,
-        ],
     )
