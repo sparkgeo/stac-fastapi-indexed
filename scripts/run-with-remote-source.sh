0da5cfdd717da31b6c1f982665b5d5a2d950af41
Tom Christian <64801328+captaincoordinates@users.noreply.github.com>
2025-04-09 05:37:56 -0700

    feat(#133): s3 index script
    * feat(#133): generic naming for all remote sources
    
    * feat(#133): remote S3 indexing

--- scripts/run-with-remote-source.sh
+++ scripts/run-with-remote-source.sh
@@ -5,7 +5,7 @@ set -e
 pushd $(dirname $0)/..
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <root-http-catalog-uri>"
+    echo "Usage: $0 <root-catalog-uri>"
     exit 1
 fi
 
@@ -19,9 +19,13 @@ fi
 
 root_catalog_uri="$1"
 
+if [[ $root_catalog_uri == s3://* ]]; then
+    echo; echo "* Assumes \$AWS_ACCESS_KEY_ID, \$AWS_REGION, \$AWS_SECRET_ACCESS_KEY, and (optionally) \$AWS_SESSION_TOKEN are set for obstore *"; echo
+fi
+
 export tmp_index_config_path=$(mktemp)
 echo "{\"root_catalog_uri\": \"$root_catalog_uri\", \"indexables\": {}, \"queryables\": {}, \"sortables\": {} $fixes_json }" > $tmp_index_config_path
-dco="docker compose -f docker-compose.base.yml -f docker-compose.remote-http.yml"
+dco="docker compose -f docker-compose.base.yml -f docker-compose.remote-source.yml"
 
 $dco build
 echo; echo "* Indexing may take some time, depending on the size of the catalog *"; echo
