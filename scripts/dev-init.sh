a179b50b8c721051d219aad9d10819699b112bbe
Tom Christian <64801328+captaincoordinates@users.noreply.github.com>
2025-04-10 07:01:52 -0700

    fix(#138): lambda resources
    * fix(#137): more appropriate permissions
    
    * fix(#138): add IAC requirements
    
    * fix(#138): bump lambda memory

--- scripts/dev-init.sh
+++ scripts/dev-init.sh
@@ -4,7 +4,7 @@ set -e
 
 pushd $(dirname $0)/..
 
-uv sync --extra dev --extra server --extra test --extra lambda
+uv sync --extra dev --extra server --extra test --extra lambda --extra iac
 pip install -r ./iac/requirements.txt
 
 uv run pre-commit install
