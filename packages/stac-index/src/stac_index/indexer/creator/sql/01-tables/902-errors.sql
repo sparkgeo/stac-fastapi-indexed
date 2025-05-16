8292c8734b2fdfc705ae3d5725bfd124582876ac
captaincoordinates <thomaschristian@gmail.com>
2025-05-15 22:36:47 -0300

    feat(#147): attempt to address bizarre timezone problem

--- packages/stac-index/src/stac_index/indexer/creator/sql/01-tables/902-errors.sql
+++ packages/stac-index/src/stac_index/indexer/creator/sql/01-tables/902-errors.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE seq_errors_id START 1;
 
 CREATE TABLE errors (
     id             INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq_errors_id'),
-    time           TIMESTAMPTZ,
+    time           TIMESTAMPTZ NOT NULL,
     error_type     VARCHAR(50) NOT NULL,
     subtype        VARCHAR NULL,
     input_location VARCHAR NULL,
